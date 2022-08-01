32ce1d24ed82be11f5162e35c37c1c132db12a41
kytosai <kytosai@gmail.com>
2022-08-01 22:42:09 +0700

    change version graphql v16 to v15

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - Video: https://www.youtube.com/watch?v=1UMNUbtzQXk
 - Repo: https://github.com/lpredrum136/reddit-fullstack-typescript-tutorial
+
+## Requirement
+
+- Must use pkg `graphql` v15 because pkg `type-graphql` don't support `graphql` v16
+  - issue: https://github.com/MichalLytek/type-graphql/issues/1100 
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,12 @@ import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express from 'express';
 import { DataSource } from 'typeorm';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import User from './entities/User';
 import Post from './entities/Post';
+import HelloResolver from './resolvers/Hello';
 
 dotenv.config();
 
@@ -21,6 +25,21 @@ const main = async () => {
   await myDataSource.initialize();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   const PORT = 4000;
   app.listen(PORT, () => {
