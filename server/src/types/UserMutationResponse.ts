c858c9ee68c718a1e380f360dee266fd06d7f5c9
kytosai <kytosai@gmail.com>
2022-08-04 22:59:04 +0700

    comment

--- server/src/types/UserMutationResponse.ts
+++ server/src/types/UserMutationResponse.ts
@@ -3,6 +3,9 @@ import { Field, InterfaceType, ObjectType } from 'type-graphql';
 import MutationResponse from './MutationReponse';
 import FieldError from './FieldError';
 
+/*
+  In video henry use `implements` MutatataionResponse instead of `extends`. However doing so would be wrong use abstract class compared to other language so i will use `extends` instead of `implements`
+*/
 @ObjectType({ implements: MutationResponse })
 class UserMutationResponse extends MutationResponse {
   @Field({ nullable: true })
