5ff10c236dc8150d98312b54d0d84a03c4836dd5
kytosai <kytosai@gmail.com>
2022-08-07 19:38:10 +0700

    connect apollo client to app

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## Requirement
 
-- Must use pkg `graphql` v15 because pkg `type-graphql` don't support `graphql` v16
+- Must use pkg `graphql` v15 in server because pkg `type-graphql` don't support `graphql` v16
   - issue: https://github.com/MichalLytek/type-graphql/issues/1100 
 - In graphql landing page. Please setting `"request.credentials": "include",` for allow save cookie from login
   - issue: https://stackoverflow.com/a/68164992 
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react';
-
 import theme from '@/theme';
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+  credentials: 'include', // ! must have to get cookie from server
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ChakraProvider>
   );
 }
