52cd5d22ed45859b88685da5f3d7ed5f7596635c
kytosai <kytosai@gmail.com>
2022-08-15 23:35:19 +0700

    fix logic check auth to redirect to login page (client)

--- client/src/utils/useCheckAuth.ts
+++ client/src/utils/useCheckAuth.ts
@@ -7,14 +7,17 @@ export const useCheckAuth = () => {
   const { data, loading } = useMeQuery();
 
   useEffect(() => {
-    if (
-      data?.me &&
-      !loading &&
-      ['/login', '/register', '/change-password', '/forgot-password'].includes(
-        router.route,
-      )
-    ) {
-      router.replace('/');
+    if (!loading) {
+      if (
+        data?.me &&
+        ['/login', '/register', '/change-password', '/forgot-password'].includes(
+          router.route,
+        )
+      ) {
+        router.replace('/');
+      } else if (!data?.me) {
+        router.replace('/login');
+      }
     }
   }, [router.route, data?.me, loading]);
 
